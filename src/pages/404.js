e3d664f9ee6e0298174a154db4ed9bbd6fa45ca0
Sushil Parajuli <sushilparajuli2010@gmail.com>
2020-02-02 12:54:15 +0400

    css added for each module with some update on page structure

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,18 @@
 import React from "react"
 import Layout from "../components/Layout"
+import Banner from "../components/Banner"
+import styles from '../css/error.module.css'
+import {Link} from 'gatsby'
 export default function error() {
   return (
-    <>
+    <main>
       <Layout>
-        <div>404</div>
+        <header className={styles.error}>
+          <Banner title="Oops! it's dead end">
+          <Link to="/" className="btn-white">Back to homepage </Link>
+        </Banner>
+        </header>
       </Layout>
-    </>
+    </main>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import SimpleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
+import {Link} from 'gatsby'
 
 export default () => (
   <Layout>
     <>
-      Hello world! <Link to="/blog">Blog </Link>
+      <SimpleHero>
+        <Banner title="This is title" info="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua">
+          <Link to="/tours" className="btn-white"> Explore Tours </Link>
+        </Banner>
+      </SimpleHero>
     </>
   </Layout>
 )
